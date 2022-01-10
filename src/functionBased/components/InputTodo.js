0f9c42c7dfa6ec946b75d80ac475abff823f4be1
benjp009 <patin.benjamin@gmail.com>
2022-01-10 17:30:36 +0100

    React function 1

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,49 +1,42 @@
-import React from "react";
+import React, { useState } from "react"
 
-const InputTodo = () => {
-  return <div></div>
+const InputTodo = props => {
+  const [inputText, setInputText] = useState({
+    title: "",
+  })
+
+  const onChange = e => {
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (inputText.title.trim()) {
+      props.addTodoProps(inputText.title)
+      setInputText({
+        title: "",
+      })
+    } else {
+      alert("Please write item")
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={inputText.title}
+        name="title"
+        onChange={onChange}
+      />
+      <button className="input-submit">Submit</button>
+    </form>
+  )
 }
 
 export default InputTodo
-
-
-// export default class InputTodo extends Component {
-//   state = {
-//     title: ""
-//   };
-//   onChange = e => {
-//     this.setState({
-//       title: e.target.value
-//     });
-//   };
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     if (this.state.title.trim()) {
-//       this.props.addTodoProps(this.state.title);
-//       this.setState({
-//         title: ""
-//       });
-//     } else {
-//       alert("Please write your todo")
-//     }
-//   };
-//   render() {
-//     return (
-//       <form
-//         onSubmit={this.handleSubmit}
-//         className="form-container">
-//         <input
-//           type="text"
-//           className="input-text"
-//           placeholder="Add your list of things to do today"
-//           onChange={this.onChange}
-//           value={this.state.title}
-//         />
-//         <button
-//           className="input-submit">
-//           Submit
-//         </button>
-//       </form>
-//     )
-//   }
-// }
