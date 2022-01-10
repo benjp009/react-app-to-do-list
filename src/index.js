6f49fd0266fb42b48e3f8c085254b72b68d41ba0
benjp009 <patin.benjamin@gmail.com>
2022-01-10 12:07:34 +0100

    Add React Strict Mode

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import ReactDOM from "react-dom"
 import TodoContainer from "./components/TodoContainer";
 
 
-ReactDOM.render(<TodoContainer />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>
+  , document.getElementById("root"));
