5c32ecc7601ba35cfdf8fd1df85bc480cb21e71d
benjp009 <patin.benjamin@gmail.com>
2022-01-10 14:33:18 +0100

    Submit new todo

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,1 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
+
+export default class InputTodo extends Component {
+  state = {
+    title: ""
+  };
+  onChange = e => {
+    this.setState({
+      title: e.target.value
+    });
+  };
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Add your list of things to do today"
+          onChange={this.onChange}
+          value={this.state.title}
+        />
+        <button>
+          Submit
+        </button>
+      </form>
+    )
+  }
+}
